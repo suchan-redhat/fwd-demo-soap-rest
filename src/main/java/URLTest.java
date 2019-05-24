47618238cf17ac69b0d6f38f0e7d0945023b14b6
sunny at lab <suchan@redhat.com>
2019-05-24 16:50:36 +0800

    add test java

---
+++ src/main/java/URLTest.java
@@ -0,0 +1,28 @@
+
+public class URLTest {
+	public static void main(String[] args) {
+		try{
+			  System.out.println("Connecting to localhost");
+			  java.net.URL url = new java.net.URL(args[1]);
+			  java.net.HttpURLConnection conn = (java.net.HttpURLConnection) url.openConnection();
+			  conn.setRequestMethod("POST");
+			  conn.setRequestProperty("Accept", "application/json");
+			  if (conn.getResponseCode() != 200) {
+			    System.out.println("ERROR");
+			  }
+			  java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream()));
+			  String output;
+			  String resultStr="";
+			  System.out.println("Out put from Server ... ... ");
+			  while ((output = br.readLine()) != null) {
+			    System.out.println(output);
+				resultStr += output;
+			  }
+			  conn.disconnect();
+		}
+		catch (Exception e){
+		  System.out.println("Exception !!!! ");
+		  e.printStackTrace();
+		}
+	}
+}
